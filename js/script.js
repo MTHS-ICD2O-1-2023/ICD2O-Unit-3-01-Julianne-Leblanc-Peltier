6a47403eb49d75484211bb8aa5aee72f56bb357f
Julianne <144264326+Julianne-Leblanc-Peltier@users.noreply.github.com>
2024-03-28 18:44:27 +0000

    area of trapezoid html js css

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Julianne Leblanc-Peltier All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Julianne Leblanc-Peltier
+// Created on: March 2024
 // This file contains the JS functions for index.html
+
+function myButtonClicked() {
+  // input
+  const aBase = parseInt(document.getElementById("a-base").value)
+  const bBase = parseInt(document.getElementById("b-base").value)
+  const height = parseInt(document.getElementById("height").value)
+
+  // process
+  const area = ((aBase + bBase) / 2) * height
+
+  // output
+  document.getElementById("area-of-trapezoid").innerHTML = area.toFixed(2) + " mm²"
+}
